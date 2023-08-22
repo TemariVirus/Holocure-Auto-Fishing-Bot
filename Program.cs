68a1bc7afbd7258848ba4810fd03003302600532
Zemogus <kiyonnxperiaz@gmail.com>
2023-08-22 16:20:02 +0800

    Make program use less CPU

--- Program.cs
+++ Program.cs
@@ -6,15 +6,19 @@ using System.IO;
 using System.Threading;
 using static WindowUtils;
 
-// TODO: Use different target areas for different buttons
+// TODO:
+// - Use different target areas for different buttons
+// - Find target circle to align the target area
 
 static class Images
 {
-    public static readonly Image2D circle = new Image2D("img/circle.png");
-    public static readonly Image2D left = new Image2D("img/left.png");
-    public static readonly Image2D right = new Image2D("img/right.png");
-    public static readonly Image2D up = new Image2D("img/up.png");
-    public static readonly Image2D down = new Image2D("img/down.png");
+    public static readonly Image2D Target = new Image2D("img/target circle.png");
+
+    public static readonly Image2D Circle = new Image2D("img/circle.png");
+    public static readonly Image2D Left = new Image2D("img/left.png");
+    public static readonly Image2D Right = new Image2D("img/right.png");
+    public static readonly Image2D Up = new Image2D("img/up.png");
+    public static readonly Image2D Down = new Image2D("img/down.png");
 }
 
 static class Program
@@ -38,11 +42,11 @@ static class Program
 
         var note_map = new Dictionary<Image2D, string>
         {
-            { Images.circle, settings.TheButtons[0] },
-            { Images.left, settings.TheButtons[2] },
-            { Images.right, settings.TheButtons[3] },
-            { Images.up, settings.TheButtons[4] },
-            { Images.down, settings.TheButtons[5] },
+            { Images.Circle, settings.TheButtons[0] },
+            { Images.Left, settings.TheButtons[2] },
+            { Images.Right, settings.TheButtons[3] },
+            { Images.Up, settings.TheButtons[4] },
+            { Images.Down, settings.TheButtons[5] },
         };
 
         IntPtr hWnd = GetHolocureWindow();
@@ -59,20 +63,22 @@ static class Program
 
         Console.WriteLine("Bot started.");
         int capture_count = 0;
-        Stopwatch sw = Stopwatch.StartNew();
+        Stopwatch perf_sw = Stopwatch.StartNew();
         Stopwatch note_timer = Stopwatch.StartNew();
         bool playing = false;
         while (true)
         {
-            // Press enter twice to Start fishing game
+            // Press confirm button twice to start fishing game
             if (!playing)
             {
+                perf_sw.Stop();
                 string key = settings.TheButtons[0];
-                Console.WriteLine($"Pressing {key}");
 
+                Console.WriteLine($"Pressing {key}");
                 InputUtils.PressKey(hWnd, key);
                 Thread.Sleep(33);
                 InputUtils.ReleaseKey(hWnd, key);
+
                 Thread.Sleep(200);
 
                 Console.WriteLine($"Pressing {key}");
@@ -82,17 +88,19 @@ static class Program
 
                 note_timer.Restart();
                 playing = true;
+                perf_sw.Start();
                 continue;
             }
 
+            // Print captures per second
             capture_count++;
-            if (sw.ElapsedMilliseconds >= 1000)
+            if (perf_sw.ElapsedMilliseconds >= 1000)
             {
                 Console.WriteLine(
-                    $"Captures per second: {capture_count / sw.Elapsed.TotalSeconds:F2}"
+                    $"Captures per second: {capture_count / perf_sw.Elapsed.TotalSeconds:F2}"
                 );
                 capture_count = 0;
-                sw.Restart();
+                perf_sw.Restart();
             }
 
             // Find note
@@ -102,7 +110,7 @@ static class Program
                 Image2D img = pair.Key;
                 string key = pair.Value;
 
-                if (target_area.MaskedContains(img))
+                if (target_area.Contains(img))
                 {
                     Console.WriteLine($"Pressing {key}");
                     InputUtils.PressKey(hWnd, key);
@@ -110,24 +118,31 @@ static class Program
                     do
                     {
                         target_area = CaptureTargetArea();
-                    } while (target_area.MaskedContains(img));
+                    } while (target_area.Contains(img));
                     InputUtils.ReleaseKey(hWnd, key);
 
                     break;
                 }
             }
 
-            if (note_timer.ElapsedMilliseconds >= 2000)
+            // If no notes for too long, restart
+            if (note_timer.ElapsedMilliseconds >= 1500)
             {
                 playing = false;
                 note_timer.Restart();
             }
+
+            // Aim for a little over 60 captures per second to match framerate
+            while (capture_count / perf_sw.Elapsed.TotalSeconds > 69)
+            {
+                Thread.Sleep(1);
+            }
         }
 
         // Laptop on 125% scale
-        Image2D CaptureTargetArea() => CaptureWindow(hWnd, 384, 280, 38, 21);
+        // Image2D CaptureTargetArea() => CaptureWindow(hWnd, 384, 280, 40, 21);
         // Desktop on 100% scale
-        // Image2D CaptureTargetArea() => CaptureWindow(hWnd, 383, 273, 36, 21);
+        Image2D CaptureTargetArea() => CaptureWindow(hWnd, 388, 273, 40, 21);
     }
 
     private static void CrashHandler(object sender, UnhandledExceptionEventArgs args)
